eb296dfc19b30705f3f178092f534f814e8e3d6f
Meryem Akay <haceryol491@gmail.com>
2022-04-22 01:01:07 +0300

    22.4.2022

--- src/src/day50_maps/C01_Maps.java
+++ src/src/day50_maps/C01_Maps.java
@@ -2,10 +2,7 @@ package src.day50_maps;
 
 import src.day49_maps.MapOlustur;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class C01_Maps {
     public static void main(String[] args) {
@@ -26,6 +23,11 @@ public class C01_Maps {
         List<Integer> keyList = new ArrayList<>();
         keyList.addAll(sınıfKeySeti);
 
+        /*
+        List<Integer> keyList = new ArrayList<>();
+        keyList.addAll(sinifList.keySet());
+         */
+
 
         System.out.println(keyList);//[101, 102, 103]
 
@@ -44,6 +46,44 @@ LinkedList<Integer> sinifKeyList= (LinkedList<Integer>) sinifList.keySet();
 sinifKeyList.get(1);
  */
 
+
+//sımdı de value lerı ındex ıle ulasabılecek bır sekle sokalım
+
+        Collection<String> sınıfvalueColl = sınıflıst.values();
+        System.out.println(sınıfvalueColl);//[Ali, Can, Dev, Veli, Yan, QA, Ali, Yan, C#]
+        System.out.println(sınıfvalueColl.size());//3
+
+        List<String> sınıfValueList = new ArrayList<>();
+        sınıfValueList.addAll(sınıfvalueColl);
+        System.out.println(sınıfValueList);//[Ali, Can, Dev, Veli, Yan, QA, Ali, Yan, C#]
+
+        /*her bır value bırden fazla bılgıyı ıcerıyor
+        onun ıcın valuelrı mult. array e atamak mantklı duruyır
+        ancak Md olsturmk ıcın boyutlrı bılmek lzm
+         */
+        int outurArrayBoyut = sınıfValueList.size();
+
+        System.out.println(outurArrayBoyut);//3
+
+        //inner arry lerın buyutunu blmak daha komplex
+        String ilkValue = sınıfValueList.get(0);
+
+        System.out.println(ilkValue);//Ali, Can, Dev
+
+        String ilkVlueArray[] = ilkValue.split(", ");
+        int innerArrayBoyut = ilkVlueArray.length;
+
+        String valueMDarr[][]=new  String[outurArrayBoyut][innerArrayBoyut];
+
+        for (int i = 0; i <outurArrayBoyut ; i++) {
+            for (int j = 0; j < innerArrayBoyut; j++) {
+                sınıfValueList.get(i).split(", ");
+
+
+
+
+            }
+        }
 
     }
 }
