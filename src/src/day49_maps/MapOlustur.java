b5f47de13282d4abc0c0c966d1f5f687623d1acb
Meryem Akay <haceryol491@gmail.com>
2022-04-23 01:58:16 +0300

    23.4.2022

--- src/src/day49_maps/MapOlustur.java
+++ src/src/day49_maps/MapOlustur.java
@@ -9,10 +9,13 @@ public class MapOlustur {
 
         Map<Integer,String> sinifList = new HashMap<>();
 
-        sinifList.put(101,"Ali, Can, Dev");
-        sinifList.put(102,"Veli, Yan, QA");
-        sinifList.put(103,"Ali, Yan, C#");
-        
+        sinifList.put(101,"Ali, Can, Dev, 1453");
+        sinifList.put(102,"Veli, Yan, QA, 1990");
+        sinifList.put(103,"Ali, Yan, C#, 2000");
+        sinifList.put(104,"Ali, san, C#, 2000");
+        sinifList.put(105,"Ali, kan, C#, 2006");
+        sinifList.put(106,"Ali, pan, C#, 2009");
+
         return sinifList;
     }
 }
--- src/src/day50_maps/C01_Maps.java
+++ src/src/day50_maps/C01_Maps.java
@@ -10,7 +10,8 @@ public class C01_Maps {
             // siniftaki ogrenci listesini duzenli olarak yazdiralim
 
             Map<Integer,String> sinifListMap= MapOlustur.myMap();
-            System.out.println(sinifListMap);
+            System.out.println(sinifListMap);//{101=Ali, Can, Dev ,1453, 102=Veli, Yan, QA,1990, 103=Ali, Yan, C#,2000}
+
 
         /* Eger key'lere tek tek ulasmak istersek
            index yapisina ihtiyacimiz var
@@ -23,16 +24,20 @@ public class C01_Maps {
 
             List<Integer> keyList=new ArrayList<>();
             keyList.addAll(sinifKeySeti);
+            System.out.println(keyList);//[101, 102, 103]
+            System.out.println(keyList.get(1));//102
 
             // Simdi de value'leri index ile ulasabilecegimiz bir sekle sokalim
 
             Collection<String> sinifValueColl =sinifListMap.values();
-            System.out.println(sinifValueColl);
+            System.out.println(sinifValueColl);//[Ali, Can, Dev ,1453, Veli, Yan, QA,1990, Ali, Yan, C#,2000]
+            System.out.println(sinifValueColl.size());//3
 
             List<String> sinifValueList=new ArrayList<>();
             sinifValueList.addAll(sinifValueColl);
 
-            System.out.println(sinifValueList);
+            System.out.println(sinifValueList);//[Ali, Can, Dev ,1453, Veli, Yan, QA,1990, Ali, Yan, C#,2000]
+
 
         /* her bir value birden fazla bilgiyi iceriyor
           onun icin value'leri multidimensional bir array'e atmak mantikli duruyor
@@ -40,13 +45,14 @@ public class C01_Maps {
 
          */
 
-            int outerArrayBoyut= sinifValueList.size();
+            int outerArrayBoyut= sinifValueList.size();//3
 
 
             // inner array'lerin boyutunu bulmak biraz daha kompleks olacak
 
             String ilkValue=sinifValueList.get(0);
-            System.out.println(ilkValue);
+            System.out.println("ilk value "+ ilkValue);//ilk value Ali, Can, Dev ,1453
+
             String ilkValueArray[]=ilkValue.split(", ");
             int innerArrayBoyut=ilkValueArray.length;
 
@@ -63,17 +69,20 @@ public class C01_Maps {
                 }
 
             }
+            System.out.println("md array " + Arrays.deepToString(valueMDArr));//md array [[Ali, Can, Dev ,1453], [Veli, Yan, QA,1990], [Ali, Yan, C#,2000]]
 
             // bu satira kadar key'leri index ile ulasabildigim keyList' e atadim
             // value'leri valueMDArr'e atadim
             // simdi bu key ve value'leri istedigim gibi manuple edebilirim
 
-            System.out.println("Numara  Isim   Soyisim  Brans");
-            System.out.println("=============================");
+            System.out.println("Numara  Isim   Soyisim  Brans  Dogum yılı");
+
+            System.out.println("==========================================");
+
             for (int i = 0; i <keyList.size() ; i++) {
-                System.out.print( keyList.get(i)+ "   ");
+                System.out.print( keyList.get(i)+ "     ");
                 for (int j = 0; j < innerArrayBoyut; j++) {
-                    System.out.print(valueMDArr[i][j] + "   ");
+                    System.out.print(valueMDArr[i][j] + "     ");
                 }
 
                 System.out.println("");
